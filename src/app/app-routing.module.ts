4ac2e5fa2ded2f9b4a17067b3045c1b19d80e337
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-07-09 22:27:48 -0300

    ajust categories route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'categories', loadChildren: './pages/categories/categories.module#CategoriesModule' }
+  { path: 'categories', loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule) }
 ];
 
 @NgModule({
