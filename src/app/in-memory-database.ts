1b772d06ea267439cf6ef4ffc967ad26f8ce38c5
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-07-12 20:11:18 -0300

    config in-memory-database for categories

---
+++ src/app/in-memory-database.ts
@@ -0,0 +1,16 @@
+import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api'
+
+export class InMemoryDatabase implements InMemoryDbService {
+
+  createDb(reqInfo?: RequestInfo): {} | import("rxjs").Observable<{}> | Promise<{}> {
+    const categories = [
+      { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
+      { id: 1, name: 'Saúde', description: 'Plano de saúde e remédios' },
+      { id: 1, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
+      { id: 1, name: 'Salário', description: 'Recebimento de salário' },
+      { id: 1, name: 'Freelas', description: 'Trabalhos como freelancer' },
+    ];
+    return { categories }
+  }
+  
+}
