b783889e9b9f7f913b862ba376f1c5d26ba60b90
Thiago Henrique <thiagonascimento95@hotmail.com>
2020-07-12 20:13:37 -0300

    create category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,9 +1,10 @@
 import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api'
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
 
   createDb(reqInfo?: RequestInfo): {} | import("rxjs").Observable<{}> | Promise<{}> {
-    const categories = [
+    const categories: Category[] = [
       { id: 1, name: 'Moradia', description: 'Pagamentos de Contas da Casa' },
       { id: 1, name: 'Saúde', description: 'Plano de saúde e remédios' },
       { id: 1, name: 'Lazer', description: 'Cinema, parques, praia, etc' },
